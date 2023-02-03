6aaaa609057d159b5bf860bfb64fe74a64aadd70
fahimsadnan99 <fahimgithub@gmail.com>
2023-02-03 19:08:37 +0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { motion } from "framer-motion"
+
+const App = () => {
+  return (
+  
+<div className='text-6xl text-red-600'>App</div>
+
+    
+  )
+}
+
+export default App
